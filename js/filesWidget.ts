272a754f44bf984d7fd0e9eaa9c21eb1b0808705
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2026-01-07 15:37:59 +0100

    Show file summary

--- js/filesWidget.ts
+++ js/filesWidget.ts
@@ -5,6 +5,9 @@ import { iconButton } from "./widgets/iconButton.ts";
 export class FilesWidget {
     element: HTMLDivElement;
     private model: AnyModel<object>;
+    private fileWidgets: [SingleFileWidget];
+    private nFilesElement: HTMLSpanElement;
+    private totalSizeElement: HTMLSpanElement;
 
     constructor(model: AnyModel<object>) {
         this.model = model;
@@ -12,10 +15,48 @@ export class FilesWidget {
         const element = document.createElement("div");
         element.classList.add("cean-files-widget");
 
-        const widget = new SingleFileWidget(model);
+        element.appendChild(this.createSummary());
+
+        const widget = new SingleFileWidget(model, () => this.updateSummary());
         element.appendChild(widget.element);
+        this.fileWidgets = [widget];
+        const widget2 = new SingleFileWidget(model, () => this.updateSummary());
+        element.appendChild(widget2.element);
+        this.fileWidgets.push(widget2);
 
         this.element = element;
+    }
+
+    private updateSummary() {
+        this.nFilesElement.textContent = this.fileWidgets
+            .filter((w) => w.size !== null)
+            .length.toString();
+
+        let size = 0;
+        for (const widget of this.fileWidgets) {
+            size += widget.size ?? 0;
+        }
+        this.totalSizeElement.textContent = humanSize(size);
+    }
+
+    private createSummary() {
+        const container = document.createElement("div");
+
+        this.nFilesElement = document.createElement("span");
+        this.nFilesElement.textContent = "0";
+        container.appendChild(this.nFilesElement);
+        const nFilesLabel = document.createElement("span");
+        nFilesLabel.textContent = "Files";
+        container.appendChild(nFilesLabel);
+
+        const sizeLabel = document.createElement("span");
+        sizeLabel.textContent = "Total Size:";
+        container.appendChild(sizeLabel);
+        this.totalSizeElement = document.createElement("span");
+        this.totalSizeElement.textContent = humanSize(0);
+        container.appendChild(this.totalSizeElement);
+
+        return container;
     }
 }
 
@@ -23,8 +64,10 @@ class SingleFileWidget {
     readonly key: string;
     readonly element: HTMLDivElement;
     private readonly responseHandler: (response: any) => void;
+    private size_: number | null = null;
+    private creationTime_: Date | null = null;
 
-    constructor(model: AnyModel<object>) {
+    constructor(model: AnyModel<object>, onChange: () => void) {
         this.key = crypto.randomUUID();
         this.element = document.createElement("div");
         this.element.id = this.key;
@@ -35,6 +78,11 @@ class SingleFileWidget {
         input.element.classList.add("cean-file-input");
         this.element.appendChild(input.element);
 
+        const removeButton = iconButton("trash", () => this.remove(model));
+        removeButton.disabled = true;
+        removeButton.classList.add("cean-remove-item");
+        this.element.appendChild(removeButton);
+
         const stats = document.createElement("div");
         stats.classList.add("cean-file-stats");
         this.element.appendChild(stats);
@@ -44,13 +92,17 @@ class SingleFileWidget {
             const payload = message.payload;
             if (payload.key !== this.key) return;
 
-            renderFileStats(payload, input, stats);
+            this.renderFileStats(payload, input, stats);
+            onChange();
         };
         model.on("msg:custom", this.responseHandler);
 
         input.element.addEventListener("input-updated", () => {
             if (input.value === null) {
                 clearFileStats(input, stats);
+                this.size_ = null;
+                this.creationTime_ = null;
+                onChange();
                 return;
             }
             model.send({
@@ -63,38 +115,56 @@ class SingleFileWidget {
         });
     }
 
+    get size(): number | null {
+        return this.size_;
+    }
+
+    get creationTime(): Date | null {
+        return this.creationTime_;
+    }
+
     private remove(model: AnyModel<object>) {
         this.element.remove();
         model.off("msg:custom", this.responseHandler);
+    }
+
+    private renderFileStats(
+        payload: any,
+        input: FileInputWidget,
+        stats: HTMLDivElement,
+    ) {
+        if (payload.success) {
+            this.size_ = payload.size;
+            this.creationTime_ = new Date(payload.creationTime);
+
+            delete input.element.dataset.error;
+
+            const sizeLabel = document.createElement("span");
+            sizeLabel.textContent = "Size:";
+            const size = document.createElement("span");
+            size.textContent = humanSize(payload.size);
+
+            const createdLabel = document.createElement("span");
+            createdLabel.textContent = "Created:";
+            const created = document.createElement("span");
+            created.textContent = this.creationTime_.toLocaleString();
+
+            stats.replaceChildren(sizeLabel, size, createdLabel, created);
+        } else {
+            this.size_ = null;
+            this.creationTime_ = null;
+
+            input.element.dataset["error"] = "true";
+
+            const span = document.createElement("span");
+            span.textContent = payload.error;
+            stats.replaceChildren(span);
+        }
     }
 }
 
 function clearFileStats(input: FileInputWidget, stats: HTMLDivElement) {
     stats.replaceChildren();
-}
-
-function renderFileStats(payload: any, input: FileInputWidget, stats: HTMLDivElement) {
-    if (payload.success) {
-        delete input.element.dataset.error;
-
-        const sizeLabel = document.createElement("span");
-        sizeLabel.textContent = "Size:";
-        const size = document.createElement("span");
-        size.textContent = humanSize(payload.size);
-
-        const createdLabel = document.createElement("span");
-        createdLabel.textContent = "Created:";
-        const created = document.createElement("span");
-        created.textContent = new Date(payload.creationTime).toLocaleString();
-
-        stats.replaceChildren(sizeLabel, size, createdLabel, created);
-    } else {
-        input.element.dataset["error"] = "true";
-
-        const span = document.createElement("span");
-        span.textContent = payload.error;
-        stats.replaceChildren(span);
-    }
 }
 
 function humanSize(size: number): string {
