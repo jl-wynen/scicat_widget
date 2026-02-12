f72ea182e24dcc49b642f5f4b3665bf0169436fc
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2026-02-12 17:08:50 +0100

    Try setting up test of widget

--- ui-tests/dataset-upload-widget.spec.ts
+++ ui-tests/dataset-upload-widget.spec.ts
@@ -16,14 +16,38 @@ test.describe("Dataset upload", () => {
         await page.notebook.openByPath(`${tmpPath}/${notebook}`);
         await page.notebook.activate(notebook);
 
-        const widget_cell = 2;
+        const widgetCell = 0;
 
-        for (let i = 0; i <= widget_cell; i++) {
+        for (let i = 0; i <= widgetCell; i++) {
             expect(await page.notebook.runCell(i));
         }
 
-        const locator = await page.notebook.getCellOutputLocator(widget_cell);
-        expect(locator !== null);
-        // locator.
+        const locator = await page.notebook.getCellOutputLocator(widgetCell);
+        if (locator === null) throw new Error("Could not find output locator");
+
+        await locator.getByLabel("Dataset name").fill("Test dataset name");
+
+        await locator.getByLabel("Creation Location").fill("TEST:widget");
+        await locator.getByLabel("PI").fill("Principal Skinner");
+        await locator.getByLabel("Contact email").fill("skinner@princi.pal");
+        // Need exact: true to avoid conflict with 'Dataset name'
+        await locator.getByLabel("Name", { exact: true }).fill("Billy Ownerson");
+
+        // TODO this fails (probably because of combobox)
+        await locator.getByLabel("Owner group").fill("135246");
+
+        await locator.getByRole("button", { name: /Files/ }).click();
+        await locator.getByLabel("Source folder").fill("/source/folder");
+
+        await locator.getByRole("button", { name: "Upload dataset" }).click();
+        await locator.getByRole("button", { name: "Upload", exact: true }).click();
+        await locator.getByRole("button", { name: "Close" }).click();
     });
 });
+
+// Navigation with tab key:
+// await page
+//     .locator("section")
+//     .filter({ hasText: "NameDescriptionProposalSelect" })
+//     .getByLabel("Name")
+//     .press("Tab");
